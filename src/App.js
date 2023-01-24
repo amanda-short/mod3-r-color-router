fd949031dabba5335c8cba5f3fab13dc89818e76
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-24 11:40:51 -0800

    links appear on header with correct url in search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
+// import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav>
+        <ul className="colorList">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/rbg/150/250/350">C1</NavLink>
+          </li>
+          <li>
+            <NavLink to="/rbg/100/200/300">C2</NavLink>
+          </li>
+          <li>
+            <NavLink to="/rbg/50/100/200">C3</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
