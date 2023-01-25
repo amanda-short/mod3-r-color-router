ca39f4c7edcbec1dc4466a1197051fa3311c7944
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-24 16:25:47 -0800

    error default added and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Header from './components/Header/Header.js';
 
 import { Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 import ColorDisplay from './components/ColorDisplay.js';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -10,12 +12,15 @@ function App() {
       <Header />
       <div className="main">
         <Switch>
-          <Route exact path="/"></Route>
+          <Route className="home" exact path="/">
+            colors colors colors...
+          </Route>
           <Route
             exact
             path="/rgb/:redValue/:greenValue/:blueValue"
             component={ColorDisplay}
           ></Route>
+          <Route path="*" component={NotFound} />
         </Switch>
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,14 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <ul className="colorList">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/rgb/21/158/26">Green</NavLink>
-          </li>
-          <li>
-            <NavLink to="/rgb/248/255/14">Yellow</NavLink>
-          </li>
-          <li>
-            <NavLink to="/rgb/255/0/230">Pink</NavLink>
-          </li>
-        </ul>
+        <div className="colorList">
+          <NavLink to="/" exact={true}>
+            Home
+          </NavLink>
+          <NavLink to="/rgb/21/158/26">Green</NavLink>
+          <NavLink to="/rgb/248/255/14">Yellow</NavLink>
+          <NavLink to="/rgb/255/0/230">Pink</NavLink>
+        </div>
       </nav>
     </header>
   );
